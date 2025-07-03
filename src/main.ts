104c1c7a6bd8ba5508cad2a54053433a94001548
AJ APARICIO <aparicio.anthonyjohn@gmail.com>
2025-07-03 18:21:43 +0800

    feat: setup web socket adapter and parser

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,17 @@ import { WsAdapter } from '@nestjs/platform-ws';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useWebSocketAdapter(new WsAdapter(app));
+  app.useWebSocketAdapter(
+    new WsAdapter(app, {
+      messageParser: (data) => {
+        try {
+          return JSON.parse(data.toString());
+        } catch (e) {
+          return data; // Return as is if parsing fails
+        }
+      },
+    }),
+  );
   await app.listen(process.env.NEST_PORT ?? 3479);
 }
 bootstrap();
