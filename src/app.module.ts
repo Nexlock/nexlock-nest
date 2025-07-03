1e396d5bf39d3614e7cf3f5e6b33980b77fb8345
AJ APARICIO <aparicio.anthonyjohn@gmail.com>
2025-07-03 18:21:30 +0800

    feat: create module for locks and esp32 module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,11 @@ import { UserModule } from './user/user.module';
 import { AdminAuthModule } from './admin-auth/admin-auth.module';
 import { SetupModule } from './setup/setup.module';
 import { AccessCodeModule } from './access-code/access-code.module';
+import { ModuleModule } from './module/module.module';
+import { LockModule } from './lock/lock.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, AdminAuthModule, SetupModule, AccessCodeModule],
+  imports: [PrismaModule, UserModule, AdminAuthModule, SetupModule, AccessCodeModule, ModuleModule, LockModule],
   controllers: [AppController],
   providers: [AppService],
 })
